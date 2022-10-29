9c7f2c8f3a3e6cbbf486905ebbee14f5d736a1f1
YuyuanW <1413733614@qq.com>
2022-10-30 00:00:20 +0800

    deploy 写错了个地方

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 rm -rf build &&
 yarn build &&
 cd build &&
-git add . -f &&
+git init &&
+git add .  &&
 git commit -m 'update' --no-verify &&
 git branch -M main &&
 git remote add origin git@github.com:YuyuanW/remember-money-user.git &&
-git push -f -u origin master && 
+git push -f -u origin main && 
 cd -
 echo https://yuyuanw.github.io/remember-money-user
